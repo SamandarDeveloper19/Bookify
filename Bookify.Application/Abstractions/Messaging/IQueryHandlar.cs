4a9ede2e6bfa7277e05047e5a3f71d5d2b109cb0
SamandarDeveloper19 <mamadiyorovsamandar19@gmail.com>
2025-10-12 23:34:14 +0500

    Application Layer

---
+++ Bookify.Application/Abstractions/Messaging/IQueryHandlar.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions.Messaging;
+
+public interface IQueryHandlar<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+    where TQuery : IQuery<TResponse>
+{
+}
