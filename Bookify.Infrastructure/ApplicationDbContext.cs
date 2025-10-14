c2d3c8f40d2a8dc01ff9e5fd9b660c6b665657a2
SamandarDeveloper19 <mamadiyorovsamandar19@gmail.com>
2025-10-14 18:34:04 +0500

    Infrastructure Layer

---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+public class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    private readonly IPublisher publisher;
+
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher)
+        : base(options)
+    {
+        this.publisher = publisher;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = base.SaveChangesAsync(cancellationToken);
+
+            await PublishDomainEventsAsync();
+
+            return await result;
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            throw new DbUpdateConcurrencyException("Concurrency exception occurred.", ex);
+        }
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await publisher.Publish(domainEvent);
+        }
+    }
+}
